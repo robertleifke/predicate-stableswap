03a54ee9654775233c255e6e1fbea189c3851bf9
Robert Leifke <r.leifke@gmail.com>
2025-05-28 11:23:27 -0400

    chore: change name

--- script/00_Counter.s.sol
+++ script/00_Counter.s.sol
@@ -6,11 +6,11 @@ import {Hooks} from "v4-core/src/libraries/Hooks.sol";
 import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
 
 import {Constants} from "./base/Constants.sol";
-import {Counter} from "../src/Counter.sol";
+import {PredicateSwap} from "../src/PredicateSwap.sol";
 import {HookMiner} from "v4-periphery/src/utils/HookMiner.sol";
 
-/// @notice Mines the address and deploys the Counter.sol Hook contract
-contract CounterScript is Script, Constants {
+/// @notice Mines the address and deploys the PredicateSwap.sol Hook contract
+contract PredicateSwapScript is Script, Constants {
     function setUp() public {}
 
     function run() public {
@@ -23,11 +23,11 @@ contract CounterScript is Script, Constants {
         // Mine a salt that will produce a hook address with the correct flags
         bytes memory constructorArgs = abi.encode(POOLMANAGER);
         (address hookAddress, bytes32 salt) =
-            HookMiner.find(CREATE2_DEPLOYER, flags, type(Counter).creationCode, constructorArgs);
+            HookMiner.find(CREATE2_DEPLOYER, flags, type(PredicateSwap).creationCode, constructorArgs);
 
         // Deploy the hook using CREATE2
         vm.broadcast();
-        Counter counter = new Counter{salt: salt}(IPoolManager(POOLMANAGER));
-        require(address(counter) == hookAddress, "CounterScript: hook address mismatch");
+        PredicateSwap predicateSwap = new PredicateSwap{salt: salt}(IPoolManager(POOLMANAGER));
+        require(address(predicateSwap) == hookAddress, "PredicateSwapScript: hook address mismatch");
     }
 }
